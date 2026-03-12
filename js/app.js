a805856a3096ed38a842a79600a817e6e8988e62
sarkarpoonam <11sarkar.poonam@gmail.com>
2026-03-13 00:39:45 +0400

    connected mock api

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const API="http://localhost:3000/packages"
+const API=https://69b31ef5e224ec066bdb7eea.mockapi.io/packages
 
 const cards=document.getElementById("cards")
 const form=document.getElementById("form")
